e882aec8d29627c48fb3971315005c42c383e3a8
Katherine Suazo <83734105+Katherine-Suazo@users.noreply.github.com>
2026-07-01 18:39:41 -0400

    Normalizar tablas BD
    - Cambio a ingles
    - Cambio de CamelCase a snake_case
    - incluido nuevos campos created_by, created_at, diagnosis_date

--- src/data/database/tablas.js
+++ src/data/database/tablas.js
@@ -1,43 +1,40 @@
 import db from './connection';
 
-
 export function crearTablas() {
-    db.execSync("PRAGMA foreign_keys = ON;")
+    db.execSync('PRAGMA foreign_keys = ON;');
 
-    // TABLA PACIENTES ---------
-    db.execSync (`
-        CREATE TABLE IF NOT EXISTS paciente(
+    db.execSync(`
+        CREATE TABLE IF NOT EXISTS professionals (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             rut TEXT NOT NULL UNIQUE,
-            nombre TEXT NOT NULL,
-            correo TEXT,
-            celular INTEGER
+            full_name TEXT NOT NULL,
+            email TEXT,
+            created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
         )
     `);
 
-    // TABLA PROFESIONAL ---------
-    db.execSync (`
-        CREATE TABLE IF NOT EXISTS profesional(
+    db.execSync(`
+        CREATE TABLE IF NOT EXISTS patients (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             rut TEXT NOT NULL UNIQUE,
-            nombre TEXT NOT NULL,
+            full_name TEXT NOT NULL,
+            email TEXT,
+            phone TEXT,
+            created_by INTEGER,
+            created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (created_by) REFERENCES professionals(id) ON DELETE RESTRICT
         )
     `);
 
-    // TABLA DIAGNOSTICO ---------
-    db.execSync (`
-        CREATE TABLE IF NOT EXISTS diagnostico(
+    db.execSync(`
+        CREATE TABLE IF NOT EXISTS diagnoses (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            puntaje INTEGER NOT NULL,
-            pacienteId INTEGER,
-            profesionalId  INTEGER,
-            FOREIGN KEY (pacienteId) REFERENCES paciente(id) ON DELETE RESTRICT,
-            FOREIGN KEY (profesionalId) REFERENCES profesional(id) ON DETELE RESTRICT
+            score INTEGER NOT NULL,
+            patient_id INTEGER NOT NULL,
+            professional_id INTEGER,
+            diagnosis_date TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE RESTRICT,
+            FOREIGN KEY (professional_id) REFERENCES professionals(id) ON DELETE RESTRICT
         )
     `);
-
 }
-
-
-// ctrl + shift + u + 60 + enter    (`)
-
